8bae865310925ac26930961e18bb53c2e670dd52
webboy <extramedia.nemanja@gmail.com>
2024-12-19 08:36:17 +0100

    Countries endpoints

--- src/resources/countries.ts
+++ src/resources/countries.ts
@@ -1,13 +1,6 @@
 import { RideLoggerClient } from "@/client";
-import { DEFAULT_CONFIG } from "@/config";
 
 export class RLCountries extends RideLoggerClient {
-    constructor(
-        baseURL: string = process.env.BASE_URL || DEFAULT_CONFIG.baseURL,
-        apiKey: string = process.env.API_KEY || DEFAULT_CONFIG.apiKey
-    ) {
-        super({ baseURL, apiKey });
-    }
 
     public async getCountries(): Promise<any> {
         this.consoleLog("Fetching countries");
