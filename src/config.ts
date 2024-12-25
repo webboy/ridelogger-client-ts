0231d5a6a4b36c30731db72855388959ccc38150
webboy <extramedia.nemanja@gmail.com>
2024-12-25 13:34:35 +0100

    Standardize classes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_CONFIG = {
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://192.168.1.111:8000',
     apiKey: null, // Replace this with your actual API key, if any
 };
